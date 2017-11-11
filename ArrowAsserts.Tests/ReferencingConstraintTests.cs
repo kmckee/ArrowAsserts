18769c901368b4401e31cc3ddca37d98e09d87d9
Kyle McKee <mckee.kyle@gmail.com>
2017-11-10 22:06:21 -0500

    Version bump

--- ArrowAsserts.Tests/ReferencingConstraintTests.cs
+++ ArrowAsserts.Tests/ReferencingConstraintTests.cs
@@ -19,5 +19,20 @@ namespace ArrowAsserts.Tests
                 Assert.That("ArrowAsserts.Tests", Is.Not.Referencing("ArrowAsserts"));
             });
         }
+
+        [Test]
+        public void A_Can_Reference_B_But_It_Does_Not()
+        {
+            Assert.Throws<AssertionException>(() =>
+            {
+                Assert.That("ArrowAsserts", Is.Referencing("ArrowAsserts.Tests"));
+            });
+        }
+
+        [Test]
+        public void A_Can_Not_Reference_B_And_It_Does_Not()
+        {
+            Assert.That("ArrowAsserts", Is.Not.Referencing("ArrowAsserts.Tests"));
+        }
     }
 }
